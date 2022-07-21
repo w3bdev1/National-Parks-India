7858bc52d9acbd5baee5ff1abe080e83095536ca
Sudeep Biswas <w3bdev@protonmail.com>
2022-07-21 21:35:53 +0530

    Leaflet Setup with OSM

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert('Hi')
+const midPoint = [25.70, 82.79]
+const map = L.map('map', {
+	minZoom: 4,
+    maxZoom: 11,
+}).setView(midPoint, 5);
+
+
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+osm.addTo(map)
+
