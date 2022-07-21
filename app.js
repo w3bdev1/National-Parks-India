2807a406fe9eb8f2ab67f3b08dedd09f0141e0e3
Sudeep Biswas <w3bdev@protonmail.com>
2022-07-21 21:48:00 +0530

    India boundary overlay added

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+/* Initialization */
 const midPoint = [25.70, 82.79]
 const map = L.map('map', {
 	minZoom: 4,
     maxZoom: 11,
 }).setView(midPoint, 5);
 
-
+/* Tile Provider */
 const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 osm.addTo(map)
 
+/* Overlay - India */
+const indiaBoundaryStyle = {
+    "color": "#ff7800",
+    "weight": 2,
+    "opacity": 1,
+	"fill": false
+};
+
+const indiaStateBoundary = L.geoJSON(indiaStates, {
+    style: indiaBoundaryStyle
+})
+
+indiaStateBoundary.addTo(map);
