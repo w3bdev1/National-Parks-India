ca9d29a5bec6ff9523db16cab9d144bca40212ca
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-24 13:13:26 +0530

    Stamen tile (light without labels) removed

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-const stamen_TerrainBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	ext: 'png'
-});
+// const stamen_TerrainBackground = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png', {
+// 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// 	subdomains: 'abcd',
+// 	ext: 'png'
+// });
 
 const cartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
@@ -77,7 +77,7 @@ npMarkers.addTo(map)
 const layerControl = L.control.layers(
 	baseMaps = {
 		"Physical Map": osm,
-		"Physical Map (without labels)": stamen_TerrainBackground,
+		// "Physical Map (without labels)": stamen_TerrainBackground,
 		"Physical Map Dark": cartoDB_DarkMatter,
 		"Physical Map Dark (without labels)": cartoDB_DarkMatterNoLabels,
 	},
